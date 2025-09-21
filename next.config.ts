3108ef39756d0c44222beab8dc2643573b0bd662
unknown <als.sanches@outlook.com>
2025-09-20 22:22:36 -0300

    ix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // 👈 isso faz o deploy não travar por causa de lint
+  },
+  // opcional, se o TypeScript também estiver barrando o build:
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
 };
 
 export default nextConfig;
